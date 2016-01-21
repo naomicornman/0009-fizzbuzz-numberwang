a1a1b4c616a248bdb53b2777e656e5b26ab8e15b
ncornman <ncornman@stanford.edu>
2016-01-20 17:10:21 -0800

    a.py

---
+++ a.py
@@ -0,0 +1,9 @@
+for num in range (1,31):
+	if num % 2 is 0 and num % 7 is 0:
+		print ("Numberwang!")
+	elif num % 2 is 0:
+		print ("Numbe")
+	elif num % 7 is 0:
+		print ("rwang")
+	else:
+		print (num)
